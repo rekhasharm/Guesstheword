1decf63a65c6deb27eb8c49e5e62671020be8705
rekhasharm <83804897+rekhasharm@users.noreply.github.com>
2022-03-09 13:45:39 +0530

    Add files via upload

--- gameLogin.js
+++ gameLogin.js
@@ -1,9 +1,22 @@
 function addUser() {
-    player1_name = document.getElementById("player1_name_input").value;
-    player2_name = document.getElementById("player2_name_input").value;
-  
-      localStorage.setItem("player1_name", player1_name);
-      localStorage.setItem("player2_name", player2_name);
-  
-      window.location = "gamePage.html";
+    if(document.getElementById("player1_name_input").value==""){
+        /*document.getElementById("username").placeholder= "Username Required";*/
+        document.getElementById("player1_name_input").placeholder= "Player1 name is required";
+        return;
+    }
+    else if(document.getElementById("player2_name_input").value==""){
+       /*document.getElementById("password").placeholder= "Password Required";*/
+       document.getElementById("player2_name_input").placeholder= "Player2 name is required";
+        return;
+    }
+    else{
+        player1_name = document.getElementById("player1_name_input").value;
+        player2_name = document.getElementById("player2_name_input").value;
+      
+          localStorage.setItem("player1_name", player1_name);
+          localStorage.setItem("player2_name", player2_name);
+      
+          window.location = "gamePage.html";
+    }
+
   }
